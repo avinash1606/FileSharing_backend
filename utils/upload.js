5025920e01ec82d63a02ae4b209a773c417e657d
Avinash Kumar Mehta <avinashkumarmehta2002@gmail.com>
2024-06-28 11:14:47 +0530

    Update upload.js

--- utils/upload.js
+++ utils/upload.js
@@ -1,7 +1,13 @@
 import multer from 'multer';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+// Get the current directory name
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const upload=multer({dest:'uploads'});
+// Define the upload directory dynamically
+const uploadDirectory = process.env.UPLOADS_DIR || path.join(__dirname, 'uploads');
 
+const upload = multer({ dest: uploadDirectory });
 
 export default upload;
